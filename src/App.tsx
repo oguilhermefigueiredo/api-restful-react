3c4cdc91a9019eb461a8260c66f6a151a430d797
oguilhermefigueiredo <negocio.guilhermefigueiredo@gmail.com>
2022-02-16 15:24:49 -0300

    State-while-revalidate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useFetch } from "./hooks/useFetch"
-import './hooks/App.css'
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import './App.css'
 
 type Repository = {
   full_name: string;
@@ -7,16 +8,22 @@ type Repository = {
 }
 
 function App() {
-  const { data: repositories, isFetching } 
-    = useFetch<Repository[]>('users/oguilhermefigueiredo/repos')
-  
+  const { data, isFetching } = useQuery<Repository[]>('repos', async () => {
+    const response = await axios.get('https://api.github.com/users/oguilhermefigueiredo/repos');
+
+    return response.data;
+  }, {
+    refetchOnWindowsFocus: false,
+    staleTime: 1000 * 60, // 1 minute
+  })
+
   return (
     <ul>
-      { isFetching && <p>Working on it...</p>}
-      {repositories?.map(repo => {
+      { isFetching && <p>Working on that...</p> }
+      {data?.map(repo => {
         return (
           <li key={repo.full_name}>
-            <strong>{repo.full_name}</strong>
+              {repo.full_name}
             <p>{repo.description}</p>
           </li>
         )
